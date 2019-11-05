7873bfe5148afd0d9b1ae27792b19d95e65fd953
David Silva <david.fernandes@aluno.ufop.edu.br>
2019-11-05 01:46:47 -0200

    refactor: User_Register inicializa Topic

--- src/main/java/implementations/dm_kernel/host/KafkaHost.java
+++ src/main/java/implementations/dm_kernel/host/KafkaHost.java
@@ -5,17 +5,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Properties;
-
-import implementations.dm_kernel.ConnectorImpl;
-import implementations.dm_kernel.MessageMetadataImpl;
-import implementations.dm_kernel.host.MainHost;
-import implementations.dm_kernel.server.MainServer;
-import implementations.sm_kernel.PacuResource;
-import implementations.util.ServerDiscovery;
-import implementations.util.IoT.CryptographyUtils;
-import interfaces.kernel.JCL_connector;
-import interfaces.kernel.JCL_message_get_host;
-import interfaces.kernel.JCL_message_metadata;
+import java.util.concurrent.CountDownLatch;
 
 //Kafka imports
 import org.apache.kafka.common.serialization.Serdes;
@@ -53,102 +43,64 @@ public class KafkaHost extends MainHost{
 		
 		serverAdd = properties.getProperty("serverMainAdd");
 		serverPort = Integer.parseInt(properties.getProperty("superPeerMainPort"));
-		System.out.println("1");
+
 		Thread kafkaRegister = new Thread() {
 			public void run() {
-				System.out.println("2");
 				Properties kafkaProperties = new Properties();
-				System.out.println("3");
+				
 				kafkaProperties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-jcl");
 				kafkaProperties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 				kafkaProperties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 				kafkaProperties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-				System.out.println("4");
-				final StreamsBuilder builder = new StreamsBuilder();
-				System.out.println("5");
-				KStream<String, String> source = builder.stream("streams-jcl-input");
 				
-				System.out.println("6");
+				final StreamsBuilder builder = new StreamsBuilder();
+				
+				KStream<String, String> source = builder.stream("streams-jcl-input");
 				
 				source.flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
 					.groupBy((key, value) -> value)
-	                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
+	                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("store"))
 	                .toStream()
-	                .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
-				
-				System.out.println("7");
+	                .to("streams-jcl-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+				final Topology topology = builder.build();
+
+		        final KafkaStreams streams = new KafkaStreams(topology, properties);
+
+		        final CountDownLatch latch = new CountDownLatch(1);
+
+		        System.out.println(topology.describe());
+
+		        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook"){
+		            @Override
+		            public void run () {
+		                streams.close();
+		                latch.countDown();
+		            }
+		        });
+		        
+		        try {
+		            streams.start();
+		            latch.await();
+		        } catch (Throwable t) {
+		            System.exit(1);
+		        }
 			}
 		};
-		kafkaRegister.start();
 		
-		Thread threadRegister = new Thread(){
-		    public void run(){
-		    	JCL_connector controlConnector = new ConnectorImpl(false);
-		    	if(!controlConnector.connect(serverAdd, serverPort,null)){
-		    		serverPort = Integer.parseInt(properties.getProperty("serverMainPort"));
-		    		boolean connected = controlConnector.connect(serverAdd, serverPort,null);
-		    		if (!connected){
-
-		    			String serverData[] = ServerDiscovery.discoverServer();
-		    			if (serverData != null){
-		    				serverAdd = serverData[0];
-		    				serverPort = Integer.parseInt(serverData[1]);
-		    				connected = controlConnector.connect(serverAdd, serverPort, null);		    						    				
-
-		    			} else if (BoardType < 4){
-	    					System.out.println("Starting JCL-Server.");
-	    						    					
-	    					Thread thread = new Thread() {
-	    				        public void run() {
-	    				        	MainServer.main(null);
-	    				        }
-	    				    };
-	    				    thread.start();
-	    					
-	    				    long startTime = System.currentTimeMillis(); //fetch starting time
-		    				serverAdd = "127.0.0.1";
-		    				while((!controlConnector.connect(serverAdd, serverPort, null)) && (System.currentTimeMillis()-startTime)<1000);		    				
-		    			}		    			
-		    		}
-		    	}
-		    	JCL_message_metadata msg = new MessageMetadataImpl();
-
-		    	msg.setType(-1);				
-				msg.setMetadados(metaData);
-				
-				boolean activateEncryption = false;
-				if (ConnectorImpl.encryption){
-					ConnectorImpl.encryption = false;
-					activateEncryption = true;
-				}
-				
-				JCL_message_get_host msgr = (JCL_message_get_host)controlConnector.sendReceiveG(msg,null);				
-				
-				if (activateEncryption)
-					ConnectorImpl.encryption = true;
-				
-				
-				
-				if((msgr.getSlaves() != null)){	
-					slaves.putAll(msgr.getSlaves());
-					slavesIDs.addAll(msgr.getSlavesIDs());
-					CryptographyUtils.setClusterPassword(msgr.getMAC());
-					
-					((PacuResource)rp).setHostIp(metaData);
-					rp.wakeup();
-					
-					if (BoardType >= 4)
-						configureBoard();
-					
-					System.out.println("HOST JCL/Kafka is OK");					 			
-				}				
-				else System.err.println("HOST JCL/Kafka NOT STARTED");
-				
-				ShutDownHook();
-				controlConnector.disconnect();
-		    }
-		  };
-		  threadRegister.start();
+		try {
+			kafkaRegister.start();
+			System
+				.out
+				.println("HOST JCL/Kafka is OK");
+		} catch(Throwable t) {
+			System
+				.err
+				.println("HOST JCL/Kafka NOT STARTED");
+			System
+				.err
+				.println(t);
+			System.exit(1);
+		}
 	}
-	
 }
--- src/main/java/implementations/dm_kernel/user/JCLKafka_FacadeImpl.java
+++ src/main/java/implementations/dm_kernel/user/JCLKafka_FacadeImpl.java
@@ -3,37 +3,37 @@ package implementations.dm_kernel.user;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
 
-import implementations.dm_kernel.ConnectorImpl;
-import interfaces.kernel.JCL_connector;
 import interfaces.kernel.JCL_message_register;
-import interfaces.kernel.JCL_result;
 
 public class JCLKafka_FacadeImpl extends JCL_FacadeImplLamb {
+
 	@Override
 	public Boolean register(String host,String port, String mac,String portS,JCL_message_register classReg){
-		try {
-			Properties properties = new Properties();
-			
-			properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-			properties.put(ProducerConfig.CLIENT_ID_CONFIG, mac);
-			properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-			properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-			
-			Thread threadConsumer = new Thread() {
-				public void run() {
-					KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
-				}	
-			};
-			threadConsumer.start();
-//			return new KafkaProducer<>(properties);
-//			return ((Boolean) result.getCorrectResult()).booleanValue();
-			return false;
 
+		try {
+			Properties kafkaProperties = new Properties();
+				
+			kafkaProperties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-jcl");
+			kafkaProperties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+			kafkaProperties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+			kafkaProperties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+				
+			final StreamsBuilder builder = new StreamsBuilder();
+				
+			KStream<String, String> source = builder.stream("streams-jcl-input");
+
+			source.to("streams-jcl");
+
+			return true;
 		} catch (Exception e) {
 
 			System.err
@@ -41,5 +41,79 @@ public class JCLKafka_FacadeImpl extends JCL_FacadeImplLamb {
 			e.printStackTrace();
 			return false;
 		}
+	}
+
+	public Object[] execute(
+		String objectNickname,
+		String host,
+		String port, 
+		String mac, 
+		String portS,
+		boolean hostChange, 
+		Object... args
+	) {
+		try {		
+			
+			Properties properties = new Properties();
+			
+			properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host + ":" + port);
+			properties.put(ProducerConfig.CLIENT_ID_CONFIG, objectNickname);
+			properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+			properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+			
+			Thread threadProducer = new Thread() {
+				public void run() {
+					KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
+					
+					try {
+						final ProducerRecord<String, String> record = new ProducerRecord<>(
+							objectNickname, 
+							objectNickname,
+							objectNickname
+						);
+
+						RecordMetadata metadata = kafkaProducer.send(record).get();
+
+					} catch(Throwable t) {
+						kafkaProducer.flush();
+						kafkaProducer.close();
+
+						System
+							.err
+							.println(t);
+					}
+				}	
+			};
+			
+			try {
+				threadProducer.start();
+				
+				return new Object[]{
+					host,
+					port,
+					mac,
+					portS
+				};
+			
+			} catch(Throwable t) {
+				System
+					.err
+					.println(t);
+				System.exit(1);
+			}
+
+		} catch (Exception e) {
+			System.err
+					.println("JCL facade problem in execute(String className, Object... args)");
+					e.printStackTrace();
+			return null;
+		}
+
+		return new Object[]{
+			host,
+			port,
+			mac,
+			portS
+		};
 	}
 }
